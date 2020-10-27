e222d2c43c1a551f8ada62156ef21cb3d3b541e1
Veenca <63163300+Veenca@users.noreply.github.com>
2020-10-27 19:25:35 +0100

    adding menu...

--- scrips/menu.js
+++ scrips/menu.js
@@ -1,28 +1,71 @@
-let categoryArray = $(".mainCategory");
-
-categoryArray.siblings().css("display", "none");
-
-categoryArray.on("click", function () {
-  let displayValue;
-  let element = $(this).siblings();
-
-  displayValue = element.css("display");
-  if (displayValue == "none") {
-    element.fadeIn(500);
-  } else {
-    element.css("display", "");
-    element.fadeOut(300);
-  }
+ //---------------LOAD JSON MENU INTO HTML---------------------------\\
+ let PizzeRosse=[];
+ let PizzeBianche=[];
+ let Ciccio=[]
+ let Stuzzicheria=[]
+ let Primi=[]
+ let IGrandiClassici=[]
+ let Secondi=[]
+ let Insalate=[]
+ let Pucce=[]
+ let Birre=[];
+ let Birreallaspina=[];
+ let Vini=[];
+ let Bibite=[];
+ $.getJSON("../JSON/menu.json", function(json) {
+  PizzeRosse=json.PizzeRosse
+  PizzeBianche=json.PizzeBianche;
+  Ciccio=json.Ciccio;
+  Stuzzicheria=json.Stuzzicheria;
+  Primi=json.Primi
+  IGrandiClassici=json.Igrandiclassici
+  Secondi=json.Secondi
+  Insalate=json.Insalate
+  Pucce=json.Pucce
+  Birre=json.Birre
+  Birreallaspina=json.Birreallaspina
+  Vini=json.Vini;
+  Bibite=json.Bibite
 });
-let specificName=categoryArray.siblings().children().children();
-let specificCategory = $(".specificList")
-//specificCategory.addClass('hide')
-//console.log(specificName(''))
-//console.log(categoryArray.siblings().children().children())
-specificCategory.children().addClass('hide')
-//specificCategory.children().hide();
+//----------Document Ready--------------\\
+$(document).ready(function(){
+  let specificCategory = $(".specificList")
+//-----------Append Array-------------------\\
+specificCategory.map(function(){
+  let ListToAppend=($(this).parent().children("p").text());
+  ListToAppend = ListToAppend.replace(/\s/g, '');
+  console.log(ListToAppend);
+ // ListToAppend="PizzeRosse"
+  $(this).append(appendArray(eval(ListToAppend)))
+ 
+});
 
+console.log(appendArray(PizzeRosse))
+
+//-----------------VISUALLY TOGGLE MENU--------------------------\\
+let categoryArray = $(".mainCategory");
+categoryArray.siblings().hide()
+categoryArray.on("click", function () {
+$(this).siblings().fadeToggle(300);
+});
+let i=0;
+let specificName=categoryArray.siblings().children().children();
+
+specificCategory.children().hide();
 specificCategory.parent().children().on("click", function () {
- console.log($(this).siblings().children())
- $(this).siblings().children().toggleClass('hide');
+ //console.log($(this).siblings().children())
+
+ $(this).siblings().children().toggle(300);
 }); 
+
+},100);
+//----Function append array------------\\
+
+function appendArray(array){
+  let formattedArray="";
+  let i=0;
+  for(i=0;i<array.length;i++){
+   formattedArray+="<li><p><b>"+array[i].nome+"</b><br> "+array[i].ingredienti+"</p></li>"
+  }
+  return formattedArray;
+}
