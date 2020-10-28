8c8bc2aec6506aef05621c4a34223fe6c412e44f
Veenca <63163300+Veenca@users.noreply.github.com>
2020-10-28 18:21:20 +0100

    updating menu

--- scrips/menu.js
+++ scrips/menu.js
@@ -1,79 +1,67 @@
- //---------------LOAD JSON MENU INTO HTML---------------------------\\
- let PizzeRosse=[];
- let PizzeBianche=[];
- let Ciccio=[]
- let Stuzzicheria=[]
- let Primi=[]
- let IGrandiClassici=[]
- let Secondi=[]
- let Insalate=[]
- let Pucce=[]
- let Birre=[];
- let Birreallaspina=[];
- let Vini=[];
- let Bibite=[];
- $.getJSON("../JSON/menu.json", function(json) {
-  PizzeRosse=json.PizzeRosse
-  PizzeBianche=json.PizzeBianche;
-  Ciccio=json.Ciccio;
-  Stuzzicheria=json.Stuzzicheria;
-  Primi=json.Primi
-  IGrandiClassici=json.Igrandiclassici
-  Secondi=json.Secondi
-  Insalate=json.Insalate
-  Pucce=json.Pucce
-  Birre=json.Birre
-  Birreallaspina=json.Birreallaspina
-  Vini=json.Vini;
-  Bibite=json.Bibite
-  console.log(PizzeRosse)
+//---------------LOAD JSON MENU INTO HTML---------------------------\\
+let PizzeRosse = [];
+let PizzeBianche = [];
+let Ciccio = []
+let Stuzzicheria = []
+let Primi = []
+let IGrandiClassici = []
+let Secondi = []
+let Insalate = []
+let Pucce = []
+let Birre = [];
+let Birreallaspina = [];
+let Vini = [];
+let Bibite = [];
+$.getJSON("../JSON/menu.json", function (json) {
+  PizzeRosse = json.PizzeRosse
+  PizzeBianche = json.PizzeBianche;
+  Ciccio = json.Ciccio;
+  Stuzzicheria = json.Stuzzicheria;
+  Primi = json.Primi
+  IGrandiClassici = json.Igrandiclassici
+  Secondi = json.Secondi
+  Insalate = json.Insalate
+  Pucce = json.Pucce
+  Birre = json.Birre
+  Birreallaspina = json.Birreallaspina
+  Vini = json.Vini;
+  Bibite = json.Bibite
+ 
 });
 //----------Document Ready--------------\\
-$(document).ready(function(){
-  let categoryArray = $(".mainCategory");
-  categoryArray.siblings().hide()
-  setTimeout(
-    function() 
-    {let specificCategory = $(".specificList")
-    //-----------Append Array-------------------\\
-    specificCategory.map(function(){
-      let ListToAppend=($(this).parent().children("p").text());
-      ListToAppend = ListToAppend.replace(/\s/g, '');
-      console.log(ListToAppend);
-     // ListToAppend="PizzeRosse"
-      $(this).append(appendArray(eval(ListToAppend)))
-     
-    });
-    
-    console.log(appendArray(PizzeRosse))
-    
-    //-----------------VISUALLY TOGGLE MENU--------------------------\\
-   
-  
-    categoryArray.on("click", function () {
-    $(this).siblings().fadeToggle(300);
-    });
-    let i=0;
-    let specificName=categoryArray.siblings().children().children();
-    
-    specificCategory.children().hide();
-    specificCategory.parent().children().on("click", function () {
-     //console.log($(this).siblings().children())
-    
-     $(this).siblings().children().toggle(300);
-    }); 
-    
-      //do something special
-    },300);
-  
-},100);
-//----Function append array------------\\
+let categoryArray = $(".mainCategory");
+categoryArray.siblings().hide()
+let specificCategory = $(".specificList")
+$(document).ready(function () {
 
-function appendArray(array){
-  let formattedArray="";
-  let i=0;
-  for(i=0;i<array.length;i++){
-   formattedArray+="<li><p><b>"+array[i].nome+"</b><br> "+array[i].ingredienti+"</p></li>"
+  setTimeout(
+    function () {
+      //-----------Append Array-------------------\\
+      specificCategory.map(function () {
+        let ListToAppend = ($(this).parent().children("p").text());
+        ListToAppend = ListToAppend.replace(/\s/g, '');
+        $(this).append(appendArray(eval(ListToAppend)))
+      });
+      //-----------------VISUALLY TOGGLE MENU--------------------------\\
+      categoryArray.on("click", function () {
+        $(this).siblings().fadeToggle(300);
+      });
+      specificCategory.children().hide();
+      specificCategory.parent().children().on("click", function () {
+        $(this).siblings().children().toggle(300);
+      });
+      //do something special
+      console.log("ok")
+    }, 325);
+
+});
+//----Functions------------\\
+
+function appendArray(array) {
+  let formattedArray = "";
+  let i = 0;
+  for (i = 0; i < array.length; i++) {
+    formattedArray += "<li><p><b>" + array[i].nome + "</b><br> " + array[i].ingredienti + "</p></li>"
   }
   return formattedArray;
 }
