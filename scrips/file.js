e49804cccc4c552d6ab54b6bbf4901fdbd3afefe
Veenca <63163300+Veenca@users.noreply.github.com>
2020-11-04 21:03:06 +0100

    updating cart

--- scrips/file.js
+++ scrips/file.js
@@ -5,23 +5,33 @@ let footer=$('footer');
 container=$('.container-xl')
 console.log(container.width())
  const x = window.matchMedia("(max-width: 576px)")
+
+let diff=$(document).height()-window.innerHeight;
+let divisione =$(document).height()/window.innerHeight;
+
+diff=(diff*(-1))*divisione
+
+const cartHeight=$('.cart-content').height();
+
 ChangeFooter(x);
 
 
 
 
 function ChangeFooter(x) {
+
     if (x.matches) { // If media query matches
         footer.css("width",window.innerWidth);
-      $('#footToAdj').css("bottom","-60px");
+      $('#footToAdj').css("bottom","-50px");
       $('#menuPush').show();
         if(footer.hasClass("d-flex")){
             footer.removeClass("d-flex")
             footer.removeClass("justify-content-around")
         }
     } else {
+      
         footer.css("width",container.width()*1);
-        $('#footToAdj').css("bottom",0);
+        $('#footToAdj').css("bottom",(0));
         $('#menuPush').hide();
         if(!footer.hasClass("d-flex")){
             footer.addClass("d-flex")
@@ -34,3 +44,13 @@ function ChangeFooter(x) {
     ChangeFooter(x);
    
 });
+
+
+//--------------RESIZE HEIGHT---------\\
+// create an Observer instance
+/* const resizeObserver = new ResizeObserver(entries => 
+    console.log('Body height changed:', entries[0].target.clientHeight)
+  )
+  
+  // start observing a DOM node
+  resizeObserver.observe(document.body) */
--- scrips/menu.js
+++ scrips/menu.js
@@ -30,18 +30,21 @@ $.getJSON("../JSON/menu.json", function (json) {
   Impasti=json.Impasti;
  
 });
-
+$('#cartCounter').text(0);
+let countItemCart=0;
 let categoryArray = $(".mainCategory");
 categoryArray.siblings().hide()
 let specificCategory = $(".specificList")
 $(document).ready(function () {
   setTimeout(
-    function () {
+    function ciao() {
+      let i=0;
       //-----------Append Array-------------------\\
-      specificCategory.map(function () {
+      specificCategory.map(function (index) {
         let ListToAppend = ($(this).parent().children("p").text());
         ListToAppend = ListToAppend.replace(/\s/g, '');
-        $(this).append(appendArray(eval(ListToAppend)))
+       // console.log(index,ListToAppend);
+        $(this).append(appendArray(eval(ListToAppend),index))
       });
       //-----------------VISUALLY TOGGLE MENU--------------------------\\
       categoryArray.on("click", function () {
@@ -51,26 +54,97 @@ $(document).ready(function () {
       specificCategory.parent().children().on("click", function () {
         $(this).siblings().children().toggle(300);
       });
-      //do something special
-      
-    }, 400);
+     $('.cartInfo').hide();
+     //submitting elmt
+     $('form').submit(function (event){
+      event.preventDefault();
+      let nome= $(this).parent().children('p').children('b').text()
+      let input=$(this).children().children('input')
+      let select=$(this).children().children('select')
+  //  console.log(input.val(),select.val()); //Debug
+      countItemCart+=parseInt(input.val(),10);
+      console.log(countItemCart)
+     appendCart(nome,input.val(),select.val(),countItemCart) //Function append cart;
+      input.val(1);
+      select.val('Classico')
+     })
+    
+    },
+     400);
 
 });
-//-----Cart Function-----\\
-cartBtn = $('.useCart');
-console.log(cartBtn.siblings());
+//------------CART--------\\
+let cartBtn = $('.useCart');
+let cartActive=false;
+
+
+let cartContainer=$('#carrello'); 
+cartContainer.hide();
 cartBtn.on("click",function(){
+ 
   $('#goToCart').toggleClass('hide');
   $(this).toggle();
+  $('.cartInfo').toggle();
   $(this).siblings('p:first').toggle();
+  cartActive=!cartActive;
+  cartContainer.fadeToggle(100);
+  //css
+  $('#footToAdj').css("bottom","-8vh");
+  if(!cartActive && !x.matches){
+    $('#footToAdj').css("bottom","0");
+  }
+  
+  //stop css
+  $('#liImpasti').toggle();
+  if(cartActive){
+   
+  }
 });
+
+
 //----Functions------------\\
 
-function appendArray(array) {
+function appendArray(array,index) {
   let formattedArray = "";
   let i = 0;
+ 
   for (i = 0; i < array.length; i++) {
-    formattedArray += "<li><p><b>" + array[i].nome + "</b><br> " + array[i].ingredienti + "</p></li>"
+   if(index<3){
+    formattedArray += "<li><p><b>" + array[i].nome + "</b><br> " + array[i].ingredienti + "</p> <form action=''><p class='cartInfo'>Quantita: <input type='number' min='1' max='99' maxlength='2' value='1'> <br> Impasto:<select class='custom-select'>\
+    <option selected value='Classico'>Classico</option>\
+      <option value='Cereali'>Cereali</option>\
+      <option value='Integrale tritordeum'>Integrale tritordeum</option>\
+      <option value='Curcuma'>Curcuma</option>\
+      <option value='Senza Lievito'>Senza Lievito</option>\
+      <option value='Senza Glutine'>Senza Glutine</option>\
+      <option value='Stesa Napoletana'>Stesa Napoletana</option>\
+      <option value='Ciabatta Romana'>Ciabatta Romana</option>\
+    </select> <button type='submit'value='Submit' ><i class='fas fa-cart-plus'></i></button></p></form></li>"
+    }else{
+      formattedArray += "<li><p><b>" + array[i].nome + "</b><br> " + array[i].ingredienti + "</p><form action=''><p class='cartInfo'>Quantita: <input type='number' min='1' max='99' maxlength='2' value='1'> <button type='submit'value='Submit' ><i class='fas fa-cart-plus'></i></button></p></form></li>"
   }
+    }  
   return formattedArray;
 }
+function appendCart(name,quantity,dough,counter){
+  if(dough){
+  $('#cartList').append("<li>"+name+" x "+quantity+" impasto: "+dough+"</li>")
+}else{
+  $('#cartList').append("<li>"+name+" x "+quantity+"</li>")
+}
+
+  $('#cartCounter').text(counter);
+}
+
+
+//CODICE BUTTATO
+
+/* if(cartActive && !x.matches){
+  $('#footToAdj').css("bottom","-8vh");
+  
+}else if(!cartActive && !x.matches){
+  $('#footToAdj').css("bottom","0");
+}
+else{
+  $('#footToAdj').css("bottom","-8vh");
+} */
